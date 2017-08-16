0c7b0d86b081418d10e8a5dbe7fb86d6b3b34004
Nikolay <NikolayS93@ya.ru>
2017-08-17 02:09:52 +0400

    hide console error if ph empty

--- assets/admin.js
+++ assets/admin.js
@@ -25,8 +25,9 @@ jQuery(document).ready(function($) {
 
   function set_focus_trigger(){
     $('input[type=\'text\'], input[type=\'number\'], textarea').on('focus', function(){
-      if($(this).val() == ''){
-        $(this).val($(this).attr('placeholder').replace('e.g. ', '').replace('к пр. ', '') );
+      var ph = $(this).attr('placeholder');
+      if($(this).val() == '' && ph){
+        $(this).val( ph.replace('e.g. ', '').replace('к пр. ', '') );
         $(this).select();
       }
     });
