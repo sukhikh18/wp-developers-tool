2519783a04298d3977fe28b927f3c0cbb653ff9f
Nikolays93 <nikolayS93@ya.ru>
2017-09-19 16:33:45 +0400

    update

--- includes/second-title.php
+++ includes/second-title.php
@@ -4,92 +4,103 @@ namespace DTools;
 /**
  * Дополнительный заголовок
  */
-
-add_action('edit_form_after_title', 'DTools\render_second_title');
-add_action('save_post', 'DTools\save_second_title');
+add_action('edit_form_after_title', __NAMESPACE__ . '\render_second_title');
+add_action('save_post', __NAMESPACE__ . '\save_second_title');
 
 function render_second_title(){
-  global $post;
+    global $post;
 
-  $val = get_post_meta($post->ID, '_second_title', true);
-  $empty = get_post_meta($post->ID, '_second_title_empty', true);
-  wp_nonce_field( 'st', 'second-title-nonce' );
-  ?>
-  <style>
-    #second-title {
-      padding: 3px 8px;
-      font-size: 1.7em;
-      line-height: 100%;
-      height: 1.7em;
-      width: 100%;
-      outline: 0;
-      margin: 0 0 3px;
-      background-color: #fff;
-    }
-    #empty-second-title {
-      position: absolute;
-      right: 8px;
-      top: 10px;
-    }
-    #empty-second-title input {
-      margin-left: 5px;
-    }
-  </style>
+    $val = get_post_meta($post->ID, '_second_title', true);
+    $empty = get_post_meta($post->ID, '_second_title_empty', true);
+    ?>
+    <style>
+        #second-title-wrap {
+            position: relative;
+        }
+        #second-title {
+            padding: 3px 8px;
+            font-size: 1.7em;
+            line-height: 100%;
+            height: 1.7em;
+            width: 100%;
+            outline: 0;
+            margin: 0 0 3px;
+            background-color: #fff;
+        }
+        #second-title-empty {
+            position: absolute;
+            right: 8px;
+            top: 10px;
+        }
+        #second-title-empty input {
+            margin-left: 5px;
+        }
+    </style>
 
-  <div id="second-title-wrap" style="position: relative;">
-    <input type="text" id="second-title" name="second-title" value="<?php echo $val;?>" placeholder="Введите дополнительный заголовок" size="25"/>
-    <label id="empty-second-title"> Пустой заголовок
-      <input type="checkbox" name="empty-second-title" value="1" class="align-right"<?php checked($empty, '1');?>></label>
-  </div>
-  <?php
+    <div id="second-title-wrap">
+        <input type="text" id="second-title" name="second-title" value="<?php echo $val;?>" placeholder="Введите дополнительный заголовок" size="25"/>
+        <label id="second-title-empty"> Пустой заголовок
+            <input type="checkbox" name="second-title-empty" value="1" class="align-right"<?php checked($empty, '1');?>>
+        </label>
+    </div>
+    <?php
+    wp_nonce_field( 'save_second_title', 'second-title-nonce' );
 }
 
 function save_second_title($post_id){
-  if ( !isset($_POST['second-title-nonce']) || ! wp_verify_nonce( $_POST['second-title-nonce'], 'st' ) )
-    return $post_id;
+    if ( !isset($_POST['second-title-nonce']) || ! wp_verify_nonce( $_POST['second-title-nonce'], 'save_second_title' ) )
+        return $post_id;
 
-    // Убедимся что поле установлено.
-  if ( ! isset( $_POST['second-title-nonce'] ) )
-    return;
+    $posted = array_filter( $_POST , 'sanitize_text_field' );
+    $posted = wp_parse_args( $posted, array(
+        'second-title' => '',
+        'second-title-empty' => '',
+        ) );
 
     // Фильтруем и записываем данные
-  update_post_meta( $post_id, '_second_title', sanitize_text_field( $_POST['second-title'] ) );
-  if( $clear = intval($_POST['empty-second-title']) )
-    update_post_meta( $post_id, '_second_title_empty', $clear );
-  else
-    delete_post_meta( $post_id, '_second_title_empty' );
+    if( $posted['second-title'] ) {
+        update_post_meta( $post_id, '_second_title', $posted['second-title'] );
+    }
+    else {
+        delete_post_meta( $post_id, '_second_title' );
+    }
+
+    if( $posted['second-title-empty'] ) {
+        update_post_meta( $post_id, '_second_title_empty', $clear );
+    }
+    else {
+        delete_post_meta( $post_id, '_second_title_empty' );
+    }
 }
 
 // фронт second_title функции (get_second_title() && the_second_title() )
-function get_second_title($id=false, $before='', $after=''){
-  global $post;
+function get_second_title($id=false, $before='', $after='') {
+    global $post;
 
-  if(!$id){
-    $post = get_post( $post );
-    $id = isset( $post->ID ) ? $post->ID : false;
-  }
+    if( $id === false ) {
+        $id = isset( $post->ID ) ? $post->ID : false;
+    }
 
-  if($id){
-    if( get_post_meta( $id, '_second_title_empty', true ) )
-      return $before . '' . $after;
+    if( $id ) {
+        if( get_post_meta( $id, '_second_title_empty', true ) )
+            return $before . '' . $after;
 
-    if( $s_title = get_post_meta($id, '_second_title', true) )
-      return $before . $s_title . $after;
-  }
+        if( $s_title = get_post_meta($id, '_second_title', true) )
+            return $before . $s_title . $after;
+    }
 
-  return false;
+    return false;
 }
-function the_second_title($id='', $before='<h1 class="entry-title">', $after='</h1>'){
+
+function the_second_title($id='', $before='<h1 class="entry-title">', $after='</h1>') {
+
   echo get_second_title($id, $before, $after);
 }
 
-add_filter( 'the_title', 'DTools\advanced_get_the_title', 10, 2 );
+add_filter( 'the_title', __NAMESPACE__ . '\advanced_get_the_title', 10, 2 );
 function advanced_get_the_title($title, $id){
-  if( is_admin() || ! is_singular() || ! in_the_loop() )
-    return $title;
+    if( is_admin() || ! is_singular() || ! in_the_loop() )
+        return $title;
 
-  if( $new_title = get_second_title($id) == 'false' ) // string
-  return '';
-
-  return $new_title ? $new_title : $title;
+    return ( $new_title = get_second_title($id) ) ? $new_title : $title;
 }
